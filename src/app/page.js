8dafb3b8184a15d79723d87fc64be4e3320364d5
Orlando Sánchez <pdigitalgroup@outlook.com>
2023-05-09 00:26:48 -0400

    sepolia network

--- src/app/page.js
+++ src/app/page.js
@@ -232,10 +232,10 @@ export default function Home() {
 
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
+    /* if (chainId !== 5) {
       window.alert("Change the network to Goerli");
       throw new Error("Change network to Goerli");
-    }
+    } */
 
     if (needSigner) {
       const signer = web3Provider.getSigner();
