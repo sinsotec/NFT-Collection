538a930a49675311308e45282d6d9a6c7e54ef64
Orlando Sánchez <pdigitalgroup@outlook.com>
2023-05-09 00:17:21 -0400

    Some mods

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+'use client'
 import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
-import styles from "../styles/Home.module.css";
+import { abi, NFT_CONTRACT_ADDRESS } from "../../constants";
+import styles from "./page.module.css";
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
